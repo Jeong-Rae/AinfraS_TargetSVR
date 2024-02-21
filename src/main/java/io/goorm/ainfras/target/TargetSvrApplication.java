b74870190ecaf7c2e94f8eeb1838b02c6b784e6e
JeongRae <kkwjdfo@gmail.com>
2024-02-21 10:16:11 +0900

    [Init] project init

---
+++ src/main/java/io/goorm/ainfras/target/TargetSvrApplication.java
@@ -0,0 +1,13 @@
+package io.goorm.ainfras.target;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetSvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TargetSvrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/goorm/ainfras/target/TargetSvrApplicationTests.java
@@ -0,0 +1,13 @@
+package io.goorm.ainfras.target;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetSvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
